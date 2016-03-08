afa455aba142d5e9f08cb53824e8dbba318f0fd3
Dan Debrunner <djd@apache.org>
2016-03-08 10:12:24 -0800

    Add tests for IotDevicePubSub application and working setup

--- apps/iot/src/main/java/quarks/apps/iot/ProxyIotDevice.java
+++ apps/iot/src/main/java/quarks/apps/iot/ProxyIotDevice.java
@@ -32,20 +32,22 @@ import quarks.topology.TStream;
 import quarks.topology.Topology;
 
 /**
- * Proxy IotDevice that uses publish-subscribe
- * and IotDevicePubSub application to communicate
- * with a single IotDevice connected to a message hub.
+ * Proxy IotDevice that uses publish-subscribe and IotDevicePubSub application
+ * to communicate with a single IotDevice connected to a message hub.
  */
 class ProxyIotDevice implements IotDevice {
-    
-	private IotDevicePubSub app;
-  
+
+    private IotDevicePubSub app;
+
     private final Topology topology;
-    
+
     /**
      * Create a proxy IotDevice
-     * @param app IotDevicePubSub application hosting the actual IotDevice.
-     * @param topology Topology of the subscribing application.
+     * 
+     * @param app
+     *            IotDevicePubSub application hosting the actual IotDevice.
+     * @param topology
+     *            Topology of the subscribing application.
      */
     ProxyIotDevice(IotDevicePubSub app, Topology topology) {
         this.app = app;
@@ -58,60 +60,57 @@ class ProxyIotDevice implements IotDevice {
     }
 
     /**
-     * Publishes events derived from {@code stream} using the
-     * topic {@link IotDevicePubSub#EVENTS} as a JsonObject
-     * containing eventId, event, and qos keys.
+     * Publishes events derived from {@code stream} using the topic
+     * {@link IotDevicePubSub#EVENTS} as a JsonObject containing eventId, event,
+     * and qos keys.
      */
     @Override
     public TSink<JsonObject> events(TStream<JsonObject> stream, Function<JsonObject, String> eventId,
             UnaryOperator<JsonObject> payload, Function<JsonObject, Integer> qos) {
-        
-        stream = stream.map(event ->
-         {
-             JsonObject publishedEvent = new JsonObject();
-             
-             publishedEvent.addProperty("eventId", eventId.apply(event));
-             publishedEvent.add("event", payload.apply(event));
-             publishedEvent.addProperty("qos", qos.apply(event));
-                         
-             return publishedEvent;
-         });
-        
+
+        stream = stream.map(event -> {
+            JsonObject publishedEvent = new JsonObject();
+
+            publishedEvent.addProperty("eventId", eventId.apply(event));
+            publishedEvent.add("event", payload.apply(event));
+            publishedEvent.addProperty("qos", qos.apply(event));
+
+            return publishedEvent;
+        });
+
         return PublishSubscribe.publish(stream, IotDevicePubSub.EVENTS, JsonObject.class);
     }
 
     /**
-     * Publishes events derived from {@code stream} using the
-     * topic {@link IotDevicePubSub#EVENTS} as a JsonObject
-     * containing eventId, event, and qos keys.
+     * Publishes events derived from {@code stream} using the topic
+     * {@link IotDevicePubSub#EVENTS} as a JsonObject containing eventId, event,
+     * and qos keys.
      */
     @Override
     public TSink<JsonObject> events(TStream<JsonObject> stream, String eventId, int qos) {
 
-        stream = stream.map(event ->
-        {
+        stream = stream.map(event -> {
             JsonObject publishedEvent = new JsonObject();
-            
+
             publishedEvent.addProperty("eventId", eventId);
             publishedEvent.add("event", event);
             publishedEvent.addProperty("qos", qos);
-                        
+
             return publishedEvent;
         });
-        
+
         return PublishSubscribe.publish(stream, IotDevicePubSub.EVENTS, JsonObject.class);
     }
-    
+
     /**
-     * Subscribes to commands.
-     * Doesn't yet support subscribing to all commands.
+     * Subscribes to commands. Doesn't yet support subscribing to all commands.
      */
     @Override
     public TStream<JsonObject> commands(String... commandIdentifiers) {
-        
+
         final String firstTopic = app.subscribeToCommand(commandIdentifiers[0]);
         TStream<JsonObject> commandsStream = PublishSubscribe.subscribe(this, firstTopic, JsonObject.class);
-        
+
         if (commandIdentifiers.length > 1) {
             Set<TStream<JsonObject>> additionalStreams = new HashSet<>();
             for (int i = 1; i < commandIdentifiers.length; i++) {
@@ -120,7 +119,7 @@ class ProxyIotDevice implements IotDevice {
             }
             commandsStream = commandsStream.union(additionalStreams);
         }
-     
+
         return commandsStream;
     }
 
---
+++ apps/iot/src/test/java/quarks/test/apps/iot/IotDevicePubSubTest.java
@@ -0,0 +1,88 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package quarks.test.apps.iot;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import com.google.gson.JsonObject;
+
+import quarks.apps.iot.IotDevicePubSub;
+import quarks.connectors.iot.IotDevice;
+import quarks.connectors.iot.QoS;
+import quarks.connectors.pubsub.service.ProviderPubSub;
+import quarks.connectors.pubsub.service.PublishSubscribeService;
+import quarks.execution.Job;
+import quarks.execution.Job.Action;
+import quarks.providers.direct.DirectProvider;
+import quarks.topology.TStream;
+import quarks.topology.Topology;
+import quarks.topology.plumbing.PlumbingStreams;
+import quarks.topology.tester.Condition;
+
+public class IotDevicePubSubTest {
+
+    
+
+    @Test
+    public void testIotDevicePubSubApp() throws Exception {
+        DirectProvider dp = new DirectProvider();
+
+        dp.getServices().addService(PublishSubscribeService.class, new ProviderPubSub());
+        
+        Topology iot = dp.newTopology("IotPubSub");
+        IotDevicePubSub iotApp = new IotDevicePubSub(new EchoIotDevice(iot));
+        
+        Topology app1 = dp.newTopology("App1");
+        
+        IotDevice app1Iot = iotApp.addIotDevice(app1);
+        
+        TStream<String> data = app1.strings("A", "B", "C");
+        
+        // Without this the tuple can be published and discarded before the
+        // subscriber is hooked up.
+        data = PlumbingStreams.blockingOneShotDelay(data, 500, TimeUnit.MILLISECONDS);
+        
+        TStream<JsonObject> events = data.map(
+                s -> {JsonObject j = new JsonObject(); j.addProperty("v", s); return j;});       
+        app1Iot.events(events, "ps1", QoS.FIRE_AND_FORGET);
+        
+        TStream<JsonObject> echoedCmds = app1Iot.commands("ec_ps1");
+        
+        TStream<String> ecs = echoedCmds.map(j -> j.getAsJsonObject(IotDevice.CMD_PAYLOAD).getAsJsonPrimitive("v").getAsString());
+        Condition<List<String>> tcEcho = app1.getTester().streamContents(ecs, "A", "B", "C"); // Expect all tuples
+        
+        Job jIot = dp.submit(iot.topology()).get();
+        Job jApp = dp.submit(app1.topology()).get();
+
+        for (int i = 0; i < 50 && !tcEcho.valid(); i++) {
+            Thread.sleep(50);
+        }
+
+        assertTrue(tcEcho.getResult().toString(), tcEcho.valid());        
+
+        jIot.stateChange(Action.CLOSE);
+        jApp.stateChange(Action.CLOSE);
+    }
+}
