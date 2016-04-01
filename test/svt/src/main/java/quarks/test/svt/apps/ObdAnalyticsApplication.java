2dd12979ea517589bfe0e787782ea54e2089ee1d
May Wone <mnwone@gmail.com>
2016-04-01 08:39:52 -0700

    Add Apache license header to ObdAnalyticsApplication.java

--- test/svt/src/main/java/quarks/test/svt/apps/ObdAnalyticsApplication.java
+++ test/svt/src/main/java/quarks/test/svt/apps/ObdAnalyticsApplication.java
@@ -1,3 +1,20 @@
+/**
+ * GPS analytics
+ * <p>
+ * Source is a stream of GPS sensor data {@link GpsSensor}
+ * <p>
+ * Here's an outline of the topology
+ * <ul>
+ * <li>Log GPS coordinates by publishing to IotF. The data may be used by a
+ * server application to display the vehicle on a map.</li>
+ * <li>Filter to detect speeds above a threshold and publish alert IotF</li>
+ * <li>Filter for GPS coordinates that are outside of a defined Geofence
+ * boundary</li>
+ * <li>Windowing to detect hard driving: hard braking or hard acceleration and
+ * publish alert to IotF</li>
+ * </ul>
+ * <p>
+ */
 package quarks.test.svt.apps;
 
 import static quarks.analytics.math3.stat.Statistic.MAX;
@@ -16,6 +33,7 @@ import quarks.samples.apps.JsonTuples;
 import quarks.samples.apps.Range;
 import quarks.samples.apps.sensorAnalytics.SensorAnalyticsApplication;
 import quarks.samples.utils.sensor.PeriodicRandomSensor;
+import quarks.test.svt.utils.sensor.gps.GpsSensor;
 import quarks.topology.TStream;
 import quarks.topology.Topology;
 import quarks.topology.plumbing.PlumbingStreams;
