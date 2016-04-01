c84f0873d1c8cbd9a6dfb76086a9d30b87228f96
May Wone <mnwone@gmail.com>
2016-04-01 08:58:31 -0700

    Add Apache header in OdbAnalyticsApplication.java

--- test/svt/src/main/java/quarks/test/svt/apps/ObdAnalyticsApplication.java
+++ test/svt/src/main/java/quarks/test/svt/apps/ObdAnalyticsApplication.java
@@ -1,6 +1,23 @@
-
+/**
+ * GPS analytics
+ * <p>
+ * Source is a stream of GPS sensor data {@link GpsSensor}
+ * <p>
+ * Here's an outline of the topology
+ * <ul>
+ * <li>Log GPS coordinates by publishing to IotF. The data may be used by a
+ * server application to display the vehicle on a map.</li>
+ * <li>Filter to detect speeds above a threshold and publish alert IotF</li>
+ * <li>Filter for GPS coordinates that are outside of a defined Geofence
+ * boundary</li>
+ * <li>Windowing to detect hard driving: hard braking or hard acceleration and
+ * publish alert to IotF</li>
+ * </ul>
+ * <p>
+ */
 package quarks.test.svt.apps;
 
+import quarks.test.svt.utils.sensor.gps.GpsSensor;
 import quarks.topology.Topology;
 public class ObdAnalyticsApplication{
 
